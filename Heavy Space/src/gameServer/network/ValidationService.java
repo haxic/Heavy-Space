11b03c33d5df621c83f6ef563ce341da457dc024
Haxic <Haxic@192.168.1.215>
2017-11-04 23:15:39 +0100

    .

--- Heavy Space/src/gameServer/network/ValidationService.java
+++ Heavy Space/src/gameServer/network/ValidationService.java
@@ -10,70 +10,79 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ValidationService {
 	IServerCommunicator serverCommuicator;
 	int timeout;
-	Set<ClientValidator> activeClientValidators = Collections.newSetFromMap(new ConcurrentHashMap<ClientValidator, Boolean>());
+	Set<ValidationTask> validationTasks = Collections.newSetFromMap(new ConcurrentHashMap<ValidationTask, Boolean>());
 	int validationCounter;
-	
+
 	public ValidationService(IServerCommunicator serverCommuicator, int timeout) {
 		this.serverCommuicator = serverCommuicator;
 		this.timeout = timeout;
 	}
 
 	public void handleNewConnection(Socket socket) {
-		ClientValidator clientValidator = new ClientValidator(socket);
+		ValidationTask validationTask = new ValidationTask(socket);
 		validationCounter++;
-		activeClientValidators.add(clientValidator);
+		validationTasks.add(validationTask);
 		try {
-			clientValidator.startValidation();
+			validationTask.startValidation();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public int getNumberOfCurrentlyActiveClientValidators() {
-		return activeClientValidators.size();
+	public int getNumberOfCurrentlyActivevalidationTasks() {
+		return validationTasks.size();
 	}
 
 	public int getValidationCounterCurrentValue() {
 		return validationCounter;
 	}
-	
-	private void handleException(ClientValidator clientValidator, IOException e) {
+
+	private void handleException(ValidationTask validationTask, IOException e) {
 		// e.printStackTrace();
-		if (!clientValidator.socketHandler.isClosed()) {
+		if (!validationTask.socketHandler.isClosed()) {
 			try {
-				clientValidator.socketHandler.close();
+				validationTask.socketHandler.close();
 			} catch (IOException e1) {
 				// e1.printStackTrace();
 			}
 		}
-		activeClientValidators.remove(clientValidator);
+		validationTasks.remove(validationTask);
 	}
 
-	private void handleInvalidConnection(ClientValidator clientValidator, String errorMessage) {
+	private void handleInvalidConnection(ValidationTask validationTask, String errorMessage) {
 		try {
-			clientValidator.socketHandler.sendData(errorMessage.getBytes());
+			validationTask.socketHandler.sendData(errorMessage.getBytes());
 		} catch (IOException e) {
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
-		activeClientValidators.remove(clientValidator);
+		if (!validationTask.socketHandler.isClosed()) {
+			try {
+				validationTask.socketHandler.close();
+			} catch (IOException e) {
+				// e.printStackTrace();
+			}
+		}
+		validationTasks.remove(validationTask);
+		
 	}
 
-	private void handleValidatedConnection(ClientValidator clientValidator, String username, String token) {
+	private void handleValidatedConnection(ValidationTask validationTask, String username, String token) {
 		byte[] invalidMessage = "Accepted: validation successful.".getBytes();
 		try {
-			clientValidator.socketHandler.sendData(invalidMessage);
+			validationTask.socketHandler.sendData(invalidMessage);
 		} catch (IOException e) {
-//			e.printStackTrace();
+			// e.printStackTrace();
 		}
-		activeClientValidators.remove(clientValidator);
+		validationTasks.remove(validationTask);
 		// gameModel.addPlayer("test").getDataTransferObject()
+		
 	}
 
-	class ClientValidator implements Runnable {
+	class ValidationTask implements Runnable {
 		Thread thread;
 		SocketHandler socketHandler;
 
-		public ClientValidator(Socket socket) {
+		public ValidationTask(Socket socket) {
 			this.socketHandler = new SocketHandler(socket);
 		}
 
